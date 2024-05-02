2fb86a8b88b7100765eea2464a10552ea41c5065
Kemi Thomas <kemothom71@gmail.com>
2024-05-01 21:21:12 -0400

    Time: 201 ms (83.69%), Space: 72.9 MB (71.07%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures, stack = []) {
+    const days = Array(temperatures.length).fill(0);
+    
+    for(let day = 0; day < temperatures.length; day++) {
+        while(canShrink(stack, temperatures, day)) {
+            const prevColdDay = stack.pop();
+            const daysToWait = (day - prevColdDay);
+            
+            days[prevColdDay] = daysToWait;
+        }
+    stack.push(day)
+     }
+    return days;
+};
+
+const canShrink = (stack, temperatures, day) => {
+    const previousDay = stack[stack.length-1];
+    const [prevTemp, currTemp] = [ temperatures[previousDay], temperatures[day]]
+    const isWarmer = prevTemp < currTemp;
+
+    return stack.length && isWarmer;
+}
