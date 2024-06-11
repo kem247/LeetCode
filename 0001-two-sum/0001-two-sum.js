828ff88c8e38cab2b3f7ccb0d4599222a37f4dc8
Kemi Thomas <kemothom71@gmail.com>
2024-06-10 20:42:37 -0400

    Time: 58 ms (74.42%), Space: 51.8 MB (15.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // 1. loop through the nums loop
-    // 2. loop again and compare the nums to the previous 1
-    // 3. if i + j =  target return the position in the array 
-    for(let i = 0 ; i < nums.length; i++) {
-       for(let j = i + 1; j < nums.length; j++) {
-          if( nums[i]  + nums[j] == target ) {
-              return [i,j]; 
-          }
-        }
-    }
+    const map = new Map();
     
+    for(let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        let key = nums[i];
+        
+        if (map.has(complement)) {
+            return [map.get(complement), i]
+        }
+        map.set(key, i)
+    }
+      return null;
 };
