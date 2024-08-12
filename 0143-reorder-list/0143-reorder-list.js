07cf26fd7ec0ad2627b10e0f4fccf581cc92aed1
Kemi Thomas <kemothom71@gmail.com>
2024-08-12 16:01:56 -0400

    Time: 87 ms (37.16%), Space: 57.4 MB (59.86%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.js
+++ 0143-reorder-list/0143-reorder-list.js
@@ -10,48 +10,43 @@
  * @return {void} Do not return anything, modify head in-place instead.
  */
 var reorderList = function(head) {
-    const mid = getMid(head)
-    const reversedFromMid = reverse(mid)
+    const mid = getMid(head);
+    const reverseFromMid = reverse(mid);
     
-    reorder(head, reversedFromMid)
-    
-
+    reorder(head, reverseFromMid);
 };
 
-
 const getMid = (head) => {
-    let [ slow, fast ] = [ head, head ];
-
-    while (fast && fast.next) {      
+    let [slow, fast] = [ head, head ];
+    
+    while( fast && fast.next) {
         slow = slow.next;
         fast = fast.next.next;
     }
-
     return slow;
 }
 
 const reverse = (head) => {
-    let [ prev, curr, next ] = [ null, head, null ];
-
-    while (curr) {                  
+    let [ prev, curr, next ] = [null, head, null]
+    
+    while(curr){
         next = curr.next;
         curr.next = prev;
-
+        
         prev = curr;
         curr = next;
-    }
-
-    return prev;
+      }
+    return prev
 }
 
 const reorder = (l1, l2) => {
-    let [ first, next, second ] = [ l1, null, l2 ];
-
-    while (second.next) {              
+    let [first, next, second] = [l1, null, l2]
+    
+    while(second.next){
         next = first.next;
         first.next = second;
         first = next;
-
+        
         next = second.next;
         second.next = first;
         second = next;
