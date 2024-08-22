1cad8be020c85237cb5a9a7a31166181ad41cff6
Kemi Thomas <kemothom71@gmail.com>
2024-08-21 22:54:24 -0400

    Time: 44 ms (96.60%), Space: 48.7 MB (97.84%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    let ans = []; 
+    for(let i = 0; i < nums1.length; i++) {
+        if(nums2.includes(nums1[i])) {
+            ans.push(nums1[i])
+        }
+    }
+    ans = [...new Set(ans)]
+    return ans
+};
